9ef16be08a5ebc96cf66b11dd66702e2d848ad2e
furkantomu <furkan38400@gmail.com>
2021-08-09 00:30:26 +0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import Stack from "./navigation/Stack";
+import { Provider } from "react-redux";
+import { reducer } from "./redux/store";
+import { persistStore, persistReducer } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import { PersistGate } from "redux-persist/integration/react";
 
-export default function App() {
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["userSignInReducer"],
+};
+
+const persitedReducer = persistReducer(persistConfig, reducer);
+
+const store = createStore(
+  persitedReducer,
+  applyMiddleware(thunk)
+);
+
+const persistedStore = persistStore(store);
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate persistor={persistedStore} loading={null}>
+        <Stack />
+      </PersistGate>
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
